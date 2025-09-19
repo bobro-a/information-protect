aa256a5c14f832c79f18cae435d59883531d63a5
bobro-a <m3_ba@mail.ru>
2025-09-19 20:08:49 +0300

    add function print bigDigit into file

--- bigdigit/bigDigit.go
+++ bigdigit/bigDigit.go
@@ -1,10 +1,7 @@
 package bigdigit
 
 import (
-	"io"
 	"math"
-	"os"
-	"strconv"
 )
 
 const (
@@ -14,55 +11,6 @@ const (
 type BigDigit struct {
 	IsNegative bool
 	Data       []int //todo сделать приватными: пока чисто для тестов
-}
-
-func SetBytes(b []byte) *BigDigit {
-	result := &BigDigit{}
-	str := string(b)
-	if str[0] == '-' {
-		result.IsNegative = true
-		str = str[1:]
-	}
-
-	var countBucket = int(math.Ceil(float64(len(str)) / float64(POW)))
-	result.Data = make([]int, countBucket)
-
-	bucket := 0
-	for i := len(str); i > 0; i -= POW {
-		var slice string
-		if i < POW {
-			slice = str[0:i]
-		} else {
-			slice = str[i-POW : i]
-		}
-		result.Data[bucket], _ = strconv.Atoi(slice)
-		bucket++
-	}
-	return result
-}
-
-func SetFile(path string) (*BigDigit, error) {
-	res := &BigDigit{}
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		return res, err
-	}
-	filesize := fileInfo.Size()
-	file, err := os.Open(path)
-	if err != nil {
-		return res, err
-	}
-	defer file.Close()
-
-	buffer := make([]byte, filesize)
-	for {
-		_, err := file.Read(buffer)
-		if err == io.EOF {
-			break
-		}
-	}
-	res = SetBytes(buffer)
-	return res, nil
 }
 
 func CmpDigit(d1 *BigDigit, d2 *BigDigit) int8 {

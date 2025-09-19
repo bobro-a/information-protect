f8e3ca55ed441b3a4c545b94ea2bebf6a170902a
bobro-a <m3_ba@mail.ru>
2025-09-19 10:40:19 +0300

    function sum

--- bigdigit/bigDigit.go
+++ bigdigit/bigDigit.go
@@ -13,7 +13,7 @@ const (
 
 type BigDigit struct {
 	isNegative bool
-	data       []int64
+	data       []int
 }
 
 func SetBytes(b []byte) *BigDigit {
@@ -25,7 +25,7 @@ func SetBytes(b []byte) *BigDigit {
 	}
 
 	var countBucket = int(math.Ceil(float64(len(str)) / float64(POW)))
-	result.data = make([]int64, countBucket)
+	result.data = make([]int, countBucket)
 
 	bucket := 0
 	for i := len(str); i > 0; i -= POW {
@@ -35,7 +35,7 @@ func SetBytes(b []byte) *BigDigit {
 		} else {
 			slice = str[i-POW : i]
 		}
-		result.data[bucket], _ = strconv.ParseInt(slice, 10, 64)
+		result.data[bucket], _ = strconv.Atoi(slice)
 		bucket++
 	}
 	return result
@@ -87,6 +87,48 @@ func CmpDigit(d1 *BigDigit, d2 *BigDigit) int8 {
 	}
 	return 0
 }
-func Sum(d1 *BigDigit, d2 *BigDigit) {
-	
+
+func sumNotNegative(d1 *BigDigit, d2 *BigDigit) *BigDigit {
+	i, j, remains := 0, 0, 0
+	var BASE = int(math.Pow10(POW))
+	size := max(len(d1.data), len(d2.data))
+	var res BigDigit
+	res.data = make([]int, size)
+	for ; i < len(d1.data) && j < len(d2.data); i, j = i+1, j+1 {
+		res.data[i] = (d1.data[i] + d2.data[j] + remains) % BASE
+		remains = (d1.data[i] + d2.data[j] + remains) / BASE
+	}
+	for ; i < len(d1.data); i++ {
+		res.data[i] = (d1.data[i] + remains) % BASE
+		remains = (d1.data[i] + remains) / BASE
+	}
+	for ; j < len(d2.data); j++ {
+		res.data[j] = (d2.data[j] + remains) % BASE
+		remains = (d2.data[j] + remains) / BASE
+	}
+	if remains != 0 {
+		res.data = append(res.data, remains)
+	}
+	return &res
+}
+
+func subNotNegative(d1 *BigDigit, d2 *BigDigit) *BigDigit {
+	var res BigDigit
+	var BASE = int(math.Pow10(POW))
+	i, j, loan := 0, 0, 0
+	size := min(len(d1.data), len(d2.data))
+	res.data = make([]int, size)
+
+	for ; i < len(d1.data) && j < len(d2.data); i, j = i+1, j+1 {
+		sub := d1.data[i] - loan - d2.data[j]
+		if sub >= 0 {
+			res.data[i] = sub
+			loan = 0
+		} else {
+			res.data[i] = sub + BASE
+			loan = 0
+		}
+	}
+	//todo
+	return &res
 }
